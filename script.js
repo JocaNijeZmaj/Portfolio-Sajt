7eea1d6638af651d289439ce05925fcbbc4c4248
JocaNijeZmaj <100790659+JocaNijeZmaj@users.noreply.github.com>
2022-05-12 19:44:42 +0200

    list
    list

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-const toggler = document.querySelector('.toggler')
-const navI = document.querySelectorAll('.nav_item')
+const toggler = document.querySelector(".toggler");
+const navI = document.querySelectorAll(".nav_item");
 
+toggler.addEventListener("click", () => {
+  navI.forEach((el) => {
+    el.classList.toggle("active");
+  });
+});
 
-toggler.addEventListener ('click', () => {
-    navI.forEach(el => {
-        el.classList.toggle('active')
-    })
-})
+new Glider(document.querySelector(".glider"), {
+  slidesToShow: 3,
+  slidesToScroll: 2,
+  draggable: true,
+  dots: ".dots",
+  arrows: {
+    prev: ".glider-prev",
+    next: ".glider-next",
+  },
+});
