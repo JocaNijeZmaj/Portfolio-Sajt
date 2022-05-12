69552047e34dca09911d5a0b33d26463146ec501
JocaNijeZmaj <100790659+JocaNijeZmaj@users.noreply.github.com>
2022-05-12 20:00:28 +0200

    12.5

--- script.js
+++ script.js
@@ -8,12 +8,36 @@ toggler.addEventListener("click", () => {
 });
 
 new Glider(document.querySelector(".glider"), {
-  slidesToShow: 3,
-  slidesToScroll: 2,
-  draggable: true,
+  // Mobile-first defaults
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  scrollLock: true,
   dots: ".dots",
   arrows: {
     prev: ".glider-prev",
     next: ".glider-next",
   },
+  responsive: [
+    {
+      // screens greater than >= 775px
+      breakpoint: 775,
+      settings: {
+        // Set to `auto` and provide item width to adjust to viewport
+        slidesToShow: "2",
+        slidesToScroll: "2",
+        itemWidth: 150,
+        duration: 0.25,
+      },
+    },
+    {
+      // screens greater than >= 1200px
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 2,
+        itemWidth: 150,
+        duration: 0.25,
+      },
+    },
+  ],
 });
